de155c7c54222a35ed2020734f768a8f1d0b7193
RealPinkRabbit <sdg0896@gmail.com>
2023-09-25 23:13:57 +0900

    몬스터가 화면 밖으로 나가지 못함

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,7 +3,7 @@
 from pico2d import *
 
 # 전역변수 선언
-BGI_WIDTH, BGI_HEIGHT = 800, 600
+BGI_WIDTH, BGI_HEIGHT = 800, 600 
 x = BGI_WIDTH//2
 y = BGI_HEIGHT//2
 dir_x = 0
@@ -12,6 +12,8 @@ open_canvas(BGI_WIDTH, BGI_HEIGHT)
 running = True
 standing_sprite_x = [0, 185, 369, 557, 743, 929, 1119, 1308]
 running_sprite_x = [0, 197, 395, 617, 809, 1003]
+monster_width = 180
+monster_height = 100
 standing_frame = 0
 running_frame = 0
 
@@ -52,17 +54,24 @@ def handle_events():
 while running:
     clear_canvas()
     tuk_ground.draw(BGI_WIDTH//2, BGI_HEIGHT//2)
+    if (x < monster_width//2):
+        x = monster_width//2
+    elif (x > BGI_WIDTH - monster_width//2):
+        x = BGI_WIDTH - monster_width//2
+    if (y < monster_height//2):
+        y = monster_height//2
+    elif (y > BGI_HEIGHT - monster_height//2):
+        y = BGI_HEIGHT - monster_height//2
     if (dir_x == 0) & (dir_y == 0):
-        monster.clip_draw(standing_sprite_x[standing_frame], 1104-120, 186, 120, x, y)
+        monster.clip_draw(standing_sprite_x[standing_frame], 1104-120, 186, 120, x, y, monster_width, monster_height)
     else:
-        monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, x, y)
-    
+        monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, x, y, monster_width, monster_height)
     update_canvas()
     handle_events()
     standing_frame = (standing_frame + 1) % 8
     running_frame = (running_frame + 1) % 6
-    x += dir_x * 5
-    y += dir_y * 5
+    x += dir_x * 10
+    y += dir_y * 10
     delay(0.05)
 
 close_canvas()
