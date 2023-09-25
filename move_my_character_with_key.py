a05edd606726294a3b5791b48d01523a7bed37c3
RealPinkRabbit <sdg0896@gmail.com>
2023-09-25 22:52:19 +0900

    상하좌우 이동 성공

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,6 +4,10 @@ from pico2d import *
 
 # 전역변수 선언
 BGI_WIDTH, BGI_HEIGHT = 800, 600
+x = BGI_WIDTH//2
+y = BGI_HEIGHT//2
+dir_x = 0
+dir_y = 0
 open_canvas(BGI_WIDTH, BGI_HEIGHT)
 running = True
 standing_sprite_x = [0, 185, 369, 557, 743, 929, 1119, 1308]
@@ -17,24 +21,46 @@ monster = load_image('tauromacis.png')
 
 # 함수 선언
 def handle_events():
-    global running
-    
+    global running, dir_x, dir_y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 # 메인함수
 while running:
     clear_canvas()
     tuk_ground.draw(BGI_WIDTH//2, BGI_HEIGHT//2)
 #    monster.clip_draw(standing_sprite_x[standing_frame], 1104-120, 186, 120, 400, 300)
-    monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, 400, 300)
+    monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, x, y)
     
     update_canvas()
     handle_events()
-    delay(0.05)
     standing_frame = (standing_frame + 1) % 8
     running_frame = (running_frame + 1) % 6
+    x += dir_x * 5
+    y += dir_y * 5
+    delay(0.05)
 
 close_canvas()
