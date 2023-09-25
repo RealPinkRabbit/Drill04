6e034e95ca670187da478f3f1f755fe446fecadf
RealPinkRabbit <sdg0896@gmail.com>
2023-09-25 22:45:38 +0900

    전체 구조 변경 2
    어쨰서인지 전역변수보다 리소스파일을 먼저 선언하면 오류 남

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,76 +1,40 @@
 
-# module_inport
-
+# module import
 from pico2d import *
 
-
-
-### 리소스파일 불러오기
-
-tuk_ground = load_image('TUK_GROUND.png')
-monster = load_image('tauromacis.png')
-
-
-
-### 전역변수 선언
-
+# 전역변수 선언
 BGI_WIDTH, BGI_HEIGHT = 800, 600
-x = BGI_WIDTH // 2
-y = BGI_HEIGHT // 2
-x_dir = 0
-y_dir = 0
 open_canvas(BGI_WIDTH, BGI_HEIGHT)
 running = True
 standing_sprite_x = [0, 185, 369, 557, 743, 929, 1119, 1308]
 running_sprite_x = [0, 197, 395, 617, 809, 1003]
+standing_frame = 0
+running_frame = 0
 
+# 리소스 파일
+tuk_ground = load_image('TUK_GROUND.png')
+monster = load_image('tauromacis.png')
 
-
-### 함수 선언/정의
-
+# 함수 선언
 def handle_events():
     global running
-    global y_dir
-    global x_dir
     
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_UP:
-                y_dir += 1
-            elif event.key == SDLK_DOWN:
-                y_dir -= 1
-            elif event.key == SDLK_RIGHT:
-                x_dir += 1
-            elif event.key == SDLK_LEFT:
-                x_dir -= 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_UP:
-                y_dir -= 1
-            elif event.key == SDLK_DOWN:
-                y_dir += 1
-            elif event.key == SDLK_RIGHT:
-                x_dir -= 1
-            elif event.key == SDLK_LEFT:
-                x_dir += 1
 
-
-### 메인함수
-            
+# 메인함수
 while running:
     clear_canvas()
     tuk_ground.draw(BGI_WIDTH//2, BGI_HEIGHT//2)
 #    monster.clip_draw(standing_sprite_x[standing_frame], 1104-120, 186, 120, 400, 300)
-    monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, x, y)
+    monster.clip_draw(running_sprite_x[running_frame], 1104-280, 193, 120, 400, 300)
     
     update_canvas()
     handle_events()
-    x += x_dir * 5
-    y += y_dir * 5
     delay(0.05)
+    standing_frame = (standing_frame + 1) % 8
+    running_frame = (running_frame + 1) % 6
 
 close_canvas()
