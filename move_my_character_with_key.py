af2b7424dc9ef513368525f94474d31f4f4ad581
RealPinkRabbit <sdg0896@gmail.com>
2023-09-25 21:13:28 +0900

    윈도우 창 닫기 성공

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,12 +4,21 @@ BGI_WIDTH, BGI_HEIGHT = 800, 600
 open_canvas(BGI_WIDTH, BGI_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 
+def handle_events():
+    global running
+    
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+
 running = True
 
 while running:
     clear_canvas()
     tuk_ground.draw(BGI_WIDTH//2, BGI_HEIGHT//2)
     update_canvas()
+    handle_events()
     delay(0.05)
 
 close_canvas()
